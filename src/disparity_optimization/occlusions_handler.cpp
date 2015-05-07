85c1a3e8cfbc5bde67c6057d36befd70ec307811
BB88 <littlebennyb@gmail.com>
2015-05-07 14:13:08 +0200

    convert filtered_disp2 to black and white

--- src/disparity_optimization/occlusions_handler.cpp
+++ src/disparity_optimization/occlusions_handler.cpp
@@ -18,19 +18,47 @@ namespace occlusions_handler {
 
     void occlusions_filler(){
 
-        Mat image = imread("/home/bene/ClionProjects/tesi_watermarking/img/disp2.png");
-        if (image.cols == 0){
-            cout << "Empty image";
-        } else {
-            int x =0;
-            int y =0;
-            Vec3b intensity = image.at<Vec3b>(y, x);
-            uchar blue = intensity.val[0];
-            uchar green = intensity.val[1];
-            uchar red = intensity.val[2];
-            cout << "blue " << static_cast<unsigned>(blue) << " " << "green " << static_cast<unsigned>(green)<< " " << "red " << static_cast<unsigned>(red);
-        }
+        Mat src1;
+        src1 = imread("/home/bene/ClionProjects/tesi_watermarking/img/filtered_disp2.png", CV_LOAD_IMAGE_COLOR);
+        namedWindow( "Original image", CV_WINDOW_AUTOSIZE );
+        imshow( "Original image", src1 );
 
+        // 3 channel image with BGR color (type 8UC3)
+        // the values can be stored in "int" or in "uchar". Here int is used.
+/*        Vec3b intensity2 = src1.at<Vec3b>(10,15);
+        int blue = intensity2.val[0];
+        int green = intensity2.val[1];
+        int red = intensity2.val[2];
+        cout << "Intensity = " << endl << " " << blue << " " << green << " " << red << endl << endl;*/
+
+// ******************* WRITE to Pixel intensity **********************
+        // This is an example in OpenCV 2.4.6.0 documentation
+/*        Mat H(10, 10, CV_64F);
+        for(int i = 0; i < H.rows; i++)
+            for(int j = 0; j < H.cols; j++)
+                H.at<double>(i,j)=1./(i+j+1);
+        cout<<H<<endl<<endl;*/
+
+        // Modify the pixels of the BGR image
+        for (int i=0; i<src1.rows; i++)
+        {
+            for (int j=0; j<src1.cols; j++)
+            {
+                if ( src1.at<Vec3b>(i,j)[0] == 255 &&  src1.at<Vec3b>(i,j)[1] == 255 &&  src1.at<Vec3b>(i,j)[2] == 0){
+                    src1.at<Vec3b>(i,j)[0] = 0;
+                    src1.at<Vec3b>(i,j)[1] = 0;
+                    src1.at<Vec3b>(i,j)[2] = 0;
+                } else{
+                    src1.at<Vec3b>(i,j)[0] = 255;
+                    src1.at<Vec3b>(i,j)[1] = 255;
+                    src1.at<Vec3b>(i,j)[2] = 255;
+                }
+            }
+        }
+        namedWindow( "Modify pixel", CV_WINDOW_AUTOSIZE );
+        imshow( "Modify pixel", src1 );
+        imwrite("/home/bene/ClionProjects/tesi_watermarking/img/filtered_bw.png", src1);
+        waitKey(0);
     }
 
 
